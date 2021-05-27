ae6a12a0685e643e5d998af6bdb658998278a447
jj2020driver <jj2020driver@gmail.com>
2021-05-27 14:39:56 +0300

    Set login form with formik

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Provider } from 'react-redux'
 import store from './redux'
 
-import Router from 'Router'
+import Router from './Router'
 
 function App() {
   return (
--- src/pages/LogIn.tsx
+++ src/pages/LogIn.tsx
@@ -2,13 +2,62 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import Container from '@material-ui/core/Container'
 import Box from '@material-ui/core/Box'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
+import { useFormik } from 'formik'
 
 const LogIn = () => {
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    onSubmit: async (values, actions) => {
+      console.log(values)
+    },
+  })
+
   return (
     <Container maxWidth="md">
       <Box py={4}>
-        <Box style={{ maxWidth: 600 }}>
+        <Box style={{ maxWidth: 550 }}>
           <Link to="/home">Home</Link>
+          <Box mt={2.5}>
+            <form onSubmit={formik.handleSubmit}>
+              <TextField
+                id="email"
+                name="email"
+                type="email"
+                onChange={formik.handleChange}
+                value={formik.values.email}
+                label="Email address"
+                fullWidth
+                margin="dense"
+              />
+              <TextField
+                id="password"
+                name="password"
+                type="password"
+                onChange={formik.handleChange}
+                value={formik.values.password}
+                label="Password"
+                fullWidth
+                margin="dense"
+              />
+              <Box mt={3.5}>
+                <Button
+                  type="submit"
+                  variant="contained"
+                  disabled={
+                    formik.isSubmitting ||
+                    (Boolean(formik?.submitCount) && !formik.isValid)
+                  }
+                >
+                  Log in
+                </Button>
+              </Box>
+            </form>
+          </Box>
         </Box>
       </Box>
     </Container>
