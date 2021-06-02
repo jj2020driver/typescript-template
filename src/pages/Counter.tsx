b14994293e35d6e2d5d685c9a20a8ac61bfb57c9
jj2020driver <jj2020driver@gmail.com>
2021-06-02 18:47:09 +0300

    Remove counter redux logic

--- src/pages/Counter.tsx
+++ src/pages/Counter.tsx
@@ -1,73 +1,18 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
-import styles from './Counter.module.css'
-
-import { useAppSelector, useAppDispatch } from '../redux/hooks'
-import {
-  decrement,
-  increment,
-  incrementByAmount,
-  incrementAsync,
-  incrementIfOdd,
-  selectCount,
-} from '../redux/reducers/counter'
+import Container from '@material-ui/core/Container'
+import Box from '@material-ui/core/Box'
 
 function Counter() {
-  const count = useAppSelector(selectCount)
-  const dispatch = useAppDispatch()
-  const [incrementAmount, setIncrementAmount] = useState('2')
-
-  const incrementValue = Number(incrementAmount) || 0
-
   return (
-    <div>
-      <div className={styles.row}>
-        <button
-          className={styles.button}
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
-          -
-        </button>
-        <span className={styles.value}>{count}</span>
-        <button
-          className={styles.button}
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
-          +
-        </button>
-      </div>
-      <div className={styles.row}>
-        <input
-          className={styles.textbox}
-          aria-label="Set increment amount"
-          value={incrementAmount}
-          onChange={(e) => setIncrementAmount(e.target.value)}
-        />
-        <button
-          className={styles.button}
-          onClick={() => dispatch(incrementByAmount(incrementValue))}
-        >
-          Add Amount
-        </button>
-        <button
-          className={styles.asyncButton}
-          onClick={() => dispatch(incrementAsync(incrementValue))}
-        >
-          Add Async
-        </button>
-        <button
-          className={styles.button}
-          onClick={() => dispatch(incrementIfOdd(incrementValue))}
-        >
-          Add If Odd
-        </button>
-      </div>
-      <div className={styles.row}>
-        <Link to="/">Log In</Link>,&nbsp;<Link to="/home">Home</Link>
-      </div>
-    </div>
+    <Container maxWidth="md">
+      <Box py={4}>
+        <Link to="/">Log in</Link>,&nbsp;<Link to="/home">Home</Link>
+        <br />
+        <br />
+        Counter
+      </Box>
+    </Container>
   )
 }
 
